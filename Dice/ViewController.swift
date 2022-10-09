ff152adcbfd2b4bb1253a7631d425991f4bd2575
Nikita Shvadlenko <Nikita.Shvadlenko@gmail.com>
2022-10-10 00:12:39 +1300

    Shake Motion

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,14 +8,32 @@ class ViewController: UIViewController {
         view = mainScreenView
     }
 
+    override func becomeFirstResponder() -> Bool {
+        true
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         mainScreenView.rollButtonDelegate = self
     }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            rollDice()
+        }
+    }
 }
 
+// MARK: - RollButtonDelegate
 extension ViewController: RollButtonDelegate {
     func mainScreenViewDidPressRollButton(_ mainScreenView: MainScreenView) {
+        rollDice()
+    }
+}
+
+// MARK: - Private Methods
+extension ViewController {
+    private func rollDice() {
         let diceOne = Int.random(in: 1...6)
         let diceTwo = Int.random(in: 1...6)
         mainScreenView.configure(
